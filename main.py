003fc9727e05a3a9ba15c6afdfdca5cf3f868944
lavander8 <lavander8@rambler.su>
2025-11-24 18:29:27 +0300

    Добавлена функция 'file_contents'

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def file_contents(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        for number, line in enumerate(f, start=1):
+            print(f"{number}. {line.strip()}")
+
+
 my_file = 'data.txt'
 
 with open(my_file, 'w', encoding='utf-8') as file:
@@ -6,18 +12,14 @@ with open(my_file, 'w', encoding='utf-8') as file:
     file.write("Эублефар\n")
 
 print("Содержимое файла:")
-with open(my_file, 'r', encoding='utf-8') as file:
-    for number, line in enumerate(file, start=1):
-        print(f"{number}. {line.strip()}")
+file_contents(my_file)
 
 with open(my_file, 'a', encoding='utf-8') as file:
     file.write("Галаго\n")
     file.write("Фенек\n")
 
 print("\nСодержимое файла после добавления:")
-with open(my_file, 'r', encoding='utf-8') as file:
-    for number, line in enumerate(file, start=1):
-        print(f"{number}. {line.strip()}")
+file_contents(my_file)
 
 copy_name = 'data_copy.txt'
 with open(my_file, 'rb') as src, open(copy_name, 'wb') as dst:
